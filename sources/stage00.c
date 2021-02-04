ae3b9439d1c2d035c9f3c3937b1fb26f4ccd0e5d
Irenee <icaroulle@gmail.com>
2021-02-04 21:17:48 +0100

    Enable texturing

--- sources/stage00.c
+++ sources/stage00.c
@@ -8,6 +8,8 @@
 #include <nusys.h>
 #include "graphic.h"
 
+#include "rgba16sign1.h"
+
 void applyMatrices(Matrices* matrices);
 
 void shadetri(Matrices* matrices);
@@ -62,10 +64,10 @@ void makeDL00(void)
 
 /* The vertex coordinate  */
 static Vtx shade_vtx[] =  {
-        {        -16,  16, -5, 0, 0, 0, 0, 0xff, 0, 0xff	},
-        {         16,  16, -5, 0, 0, 0, 0, 0, 0, 0xff	},
-        {         16, -16, -5, 0, 0, 0, 0, 0, 0xff, 0xff	},
-        {        -16, -16, -5, 0, 0, 0, 0xff, 0, 0, 0xff	},
+        {        -16,  16, -5, 0, 31<<6,  0<<6, 0x00, 0xff, 0x00, 0xff	},
+        {         16,  16, -5, 0,  0<<0,  0<<6, 0x00, 0x00, 0x00, 0xff	},
+        {         16, -16, -5, 0,  0<<6, 31<<6, 0x00, 0x00, 0xff, 0xff	},
+        {        -16, -16, -5, 0, 31<<6, 31<<6, 0xff, 0x00, 0x00, 0xff	},
 };
 
 void applyMatrices(Matrices* matrices)
@@ -75,12 +77,10 @@ void applyMatrices(Matrices* matrices)
     OS_K0_TO_PHYSICAL(&(matrices->rotation)),
 		G_MTX_MODELVIEW|G_MTX_LOAD|G_MTX_NOPUSH);
 
-#if 1
   gSPMatrix(
     glistp++,
     OS_K0_TO_PHYSICAL(&(matrices->translation)),
 		G_MTX_MODELVIEW|G_MTX_MUL|G_MTX_NOPUSH);
-#endif
 }
 
 /* Draw a square  */
@@ -92,7 +92,41 @@ void shadetri(Matrices* matrices)
 
   gDPPipeSync(glistp++);
   gDPSetCycleType(glistp++,G_CYC_1CYCLE);
+
+  // Set Texture
+  /* Enable texture, set scaling parameters */
+  gSPTexture(glistp++, 0x8000, 0x8000, 0, G_TX_RENDERTILE, G_ON);
+
+  /* Specify back surface culling */
+  gSPSetGeometryMode(glistp++, G_CULL_BACK);
+
+  /* Switch to combine mode using texture color */
+  gDPSetCombineMode(glistp++, G_CC_DECALRGBA, G_CC_DECALRGBA);
+
+  /* Load texture (image rgba16sign1) */
+  gDPLoadTextureBlock(glistp++,
+                      rgba16sign1,              /* Pointer to texture image */
+                      G_IM_FMT_RGBA,            /* Texel format */
+                      G_IM_SIZ_16b,             /* Texel size */
+                      32, 32,                   /* Image width and height */
+                      0,                        /* LUT (palette) index (not used here) */
+                      G_TX_WRAP, G_TX_WRAP,     /* Clamp, wrap, mirror frag in s direction */
+                      5, 5,                     /* s, t masks */
+                      G_TX_NOLOD, G_TX_NOLOD);  /* Shift (not shifted here) */
+
+  /* Texture perspective correction is turned on during mapping */
+  gDPSetTexturePersp(glistp++, G_TP_PERSP);
+  /* Set texture filter */
+  gDPSetTextureFilter(glistp++, G_TF_BILERP);
+  gDPSetTextureConvert(glistp++, G_TC_FILT);
+  /* This can be ignored until LOD or detail texture is explained */
+  gDPSetTextureLOD(glistp++, G_TL_TILE);
+  gDPSetTextureDetail(glistp++, G_TD_CLAMP);
+  /* Texture palette (not used) */
+  gDPSetTextureLUT(glistp++, G_TT_NONE);
+
   gDPSetRenderMode(glistp++,G_RM_AA_OPA_SURF, G_RM_AA_OPA_SURF2);
+
   gSPClearGeometryMode(glistp++,0xFFFFFFFF);
   gSPSetGeometryMode(glistp++,G_SHADE| G_SHADING_SMOOTH);
 
