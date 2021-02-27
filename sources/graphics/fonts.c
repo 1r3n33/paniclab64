f747c43cde4faa1a2f2e11d00259e3a767c0d2f4
Irenee <icaroulle@gmail.com>
2021-02-27 16:37:19 +0100

    Pixel perfect text rendering

--- sources/graphics/fonts.c
+++ sources/graphics/fonts.c
@@ -1,8 +1,37 @@
 #include "fonts.h"
 
-#include "../../assets/graphics/fonts_64x64_I_4b.h"
+#include "../../assets/graphics/fonts_128x64_I_4b.h"
 
-Mtx fonts_mtx;
+#define MAKE_U(u) (((u - 1) << 6) | (1 << 5))
+#define MAKE_V(v) (((v - 1) << 6) | (1 << 5))
+
+#define GLYPH(w, h, t, l, b, r)                                         \
+    {                                                                   \
+        {0, h, 0, 0, MAKE_U(l), MAKE_V(t), 0xff, 0xff, 0xff, 0xff},     \
+            {w, h, 0, 0, MAKE_U(r), MAKE_V(t), 0xff, 0xff, 0xff, 0xff}, \
+            {w, 0, 0, 0, MAKE_U(r), MAKE_V(b), 0xff, 0xff, 0xff, 0xff}, \
+            {0, 0, 0, 0, MAKE_U(l), MAKE_V(b), 0xff, 0xff, 0xff, 0xff}, \
+    }
+
+Mtx glyph_mtxs[32];
+
+static Vtx glyphes[128][4] = {
+    ['A'] = GLYPH(5, 10, 1, 1, 11, 6),
+    ['B'] = GLYPH(5, 10, 1, 7, 11, 12),
+    ['C'] = GLYPH(5, 10, 1, 13, 11, 18),
+    ['U'] = GLYPH(5, 10, 12, 1, 22, 6),
+    ['V'] = GLYPH(5, 10, 12, 7, 22, 12),
+    ['W'] = GLYPH(7, 10, 12, 13, 22, 20),
+};
+
+static u32 spaces[128] = {
+    ['A'] = 5 + 1,
+    ['B'] = 5 + 1,
+    ['C'] = 5 + 1,
+    ['U'] = 5 + 1,
+    ['V'] = 5 + 1,
+    ['W'] = 7 + 1,
+};
 
 Gfx *apply_fonts_texture(Gfx *glistp)
 {
@@ -13,13 +42,13 @@ Gfx *apply_fonts_texture(Gfx *glistp)
     gDPSetCombineMode(glistp++, G_CC_DECALRGBA, G_CC_DECALRGBA);
 
     gDPLoadTextureBlock_4b(glistp++,
-                           _pp_table_fonts_64x64_I_4b->pixel.p4, // Pointer to texture image
-                           G_IM_FMT_I,                           // Texel format
-                           64, 64,                               // Image width and height
-                           0,                                    // LUT (palette) index
-                           G_TX_WRAP, G_TX_WRAP,                 // Clamp, wrap, mirror frag in s direction
-                           6, 6,                                 // s, t masks
-                           G_TX_NOLOD, G_TX_NOLOD);              // Shift (not shifted here)
+                           _pp_table_fonts_128x64_I_4b->pixel.p4, // Pointer to texture image
+                           G_IM_FMT_I,                            // Texel format
+                           128, 64,                               // Image width and height
+                           0,                                     // LUT (palette) index
+                           G_TX_WRAP, G_TX_WRAP,                  // Clamp, wrap, mirror frag in s direction
+                           7, 6,                                  // s, t masks
+                           G_TX_NOLOD, G_TX_NOLOD);               // Shift (not shifted here)
 
     // Texture palette
     gDPSetTextureLUT(glistp++, G_TT_NONE);
@@ -27,7 +56,7 @@ Gfx *apply_fonts_texture(Gfx *glistp)
     // Texture perspective correction is turned on during mapping
     gDPSetTexturePersp(glistp++, G_TP_PERSP);
     // Set texture filter
-    gDPSetTextureFilter(glistp++, G_TF_BILERP);
+    gDPSetTextureFilter(glistp++, G_TF_POINT);
     gDPSetTextureConvert(glistp++, G_TC_FILT);
 
     // This can be ignored until LOD or detail texture is explained
@@ -50,13 +79,6 @@ Gfx *render_glyph(Gfx *glistp, Mtx *m, Vtx *vtx)
     return glistp;
 }
 
-static Vtx zero[] = {
-    {0, 11, 0, 0, 34 << 6, 32 << 6, 0xff, 0xff, 0xff, 0xff},
-    {8, 11, 0, 0, 41 << 6, 32 << 6, 0xff, 0xff, 0xff, 0xff},
-    {8, 0, 0, 0, 41 << 6, 43 << 6, 0xff, 0xff, 0xff, 0xff},
-    {0, 0, 0, 0, 34 << 6, 43 << 6, 0xff, 0xff, 0xff, 0xff},
-};
-
 Gfx *render_string(Gfx *glistp, char *str, f32 x, f32 y)
 {
     glistp = apply_fonts_texture(glistp);
@@ -69,12 +91,13 @@ Gfx *render_string(Gfx *glistp, char *str, f32 x, f32 y)
     gSPClearGeometryMode(glistp++, 0xFFFFFFFF);
     gSPSetGeometryMode(glistp++, G_SHADE | G_SHADING_SMOOTH);
 
-    guTranslate(&fonts_mtx, x, y, 0.0f);
-
-    while (*str)
+    u32 i = 0;
+    while (str[i])
     {
-        glistp = render_glyph(glistp, &fonts_mtx, zero);
-        str++;
+        guTranslate(&glyph_mtxs[i], x, y, 0.0f);
+        glistp = render_glyph(glistp, &glyph_mtxs[i], glyphes[str[i]]);
+        x += spaces[str[i]];
+        i++;
     }
 
     return glistp;
